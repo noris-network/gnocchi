09b4318657d2247748d673e945932abb8a5a896d
Fabian Zimmermann <fabian.zimmermann@noris.de>
2019-03-12 12:51:44 +0100

    also dont log "201 created" requests

--- gnocchi/rest/app.py
+++ gnocchi/rest/app.py
@@ -64,7 +64,7 @@ class GnocchiHook(pecan.hooks.PecanHook):
 
     @staticmethod
     def after(state):
-        if state.response.status_code not in [200, 202]:
+        if state.response.status_code not in [200, 201, 202]:
             LOG.debug("Request:\n%s\n", state.request)
             LOG.debug("Response:\n%s\n", state.response)
 
